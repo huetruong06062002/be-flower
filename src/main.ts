cab76631d6b1f159a33c1131bab2c15fdc3afdc4
huetruong <truongvshieu01@gmail.com>
2024-10-22 14:28:10 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
+import * as process from "process";
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
